8bcaeb9c8d514ac32eb0613dbaf0f01549bc1bb9
onerbdev <breno.022806@gmail.com>
2026-05-23 17:38:13 -0300

    Fix: Aplicado encapsulamento completo e adicionado gitignore

--- sistemabancario/ContaBancaria.java
+++ sistemabancario/ContaBancaria.java
@@ -5,8 +5,8 @@ public class ContaBancaria {
 	//Atributos: As informações que a conta guarda.
 	//Usamos 'public' para indicar que qualquer um pode mexer.
 	
-	public String titular;
-	public double saldo;
+	private String titular;
+	private double saldo;
 	
 	//Construtor: O método que cria a conta na memória.
 	//Toda vez que damos um 'new ContaBancaria', o código abaixo roda.
@@ -21,7 +21,16 @@ public class ContaBancaria {
 	public void depositar(double valor) {
 		if (valor > 0) {
 			this.saldo += valor;
-			System.out.println("Depósito de R$ " + valor + "realizado.");
+			System.out.println("Depósito de R$ " + valor + " realizado.");
 		}
 	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	// Método Get para o titular: permite que a Main saiba quem é o dono da conta
+    public String getTitular() {
+        return this.titular;
+    }
 }
--- sistemabancario/Main.java
+++ sistemabancario/Main.java
@@ -3,20 +3,12 @@ package sistemabancario;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		//1. Criamos a conta do Breno usando o Construtor
 		ContaBancaria minhaConta = new ContaBancaria("Breno");
 		
-		//2. Usamos o método oficial para colocar dinheiro
+		System.out.println("Bem-vindo ao sistema, " + minhaConta.getTitular() + "!");
+		
 		minhaConta.depositar(100.0);
-		System.out.println("Saldo após depósito: R$ " + minhaConta.saldo);
-		
-		System.out.println("\n--- A INVASÃO ---");
-		
-		//3. Como o atributo é 'public', qualquer um pode burlar as regras
-		minhaConta.saldo = 10000000.0; //Injeção direta de dinheiro sem depósito
-		
-		System.out.println("Novo saldo (BURLADO): R$ " + minhaConta.saldo);
+		System.out.println("Saldo oficial verificado: R$ " + minhaConta.getSaldo());
 	}
 
 }
